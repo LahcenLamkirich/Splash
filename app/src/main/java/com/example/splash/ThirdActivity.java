63e8b9741c2995f5346f0224085fe820b1c5c298
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-03-13 03:59:37 +0100

    commit

--- app/src/main/java/com/example/splash/ThirdActivity.java
+++ app/src/main/java/com/example/splash/ThirdActivity.java
@@ -3,12 +3,18 @@ package com.example.splash;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    TextView usernamePlace, passwordPlace, txt1, txt2;
+    Button btnBack ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,26 @@ public class ThirdActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
         );
 
+        usernamePlace = findViewById(R.id.userPlace);
+        passwordPlace = findViewById(R.id.passwPlace);
+        txt1 = findViewById(R.id.txt1);
+        txt2= findViewById(R.id.txt2);
+        txt1.setPaintFlags(txt1.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        txt2.setPaintFlags(txt2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        Bundle b = getIntent().getExtras();
+        String username = b.getString("username");
+        String password = b.getString("password");
+
+        usernamePlace.setText(username);
+        passwordPlace.setText(password);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
